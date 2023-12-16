10e43b613fdf4ed036f55f72e312c16101de83d2
Nguyen Huu Quang <ducy357@gmail.com>
2023-12-16 16:55:37 +0700

    Complete profile project

--- index.js
+++ index.js
@@ -27,9 +27,150 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-const projectBtn=document.querySelector("#projectBtn");
+const projectBtn = document.querySelector("#projectBtn");
 
-projectBtn.addEventListener("click",function(){
+projectBtn.addEventListener("click", function () {
   // scroll to #project
-  document.querySelector("#projects").scrollIntoView({behavior:"smooth"});
+  document.querySelector("#projects").scrollIntoView({ behavior: "smooth" });
 });
+
+const TL = gsap.timeline();
+
+TL.from(
+  ".banner-topbar",
+  {
+    autoAlpha: 0,
+    duration: 1,
+    x: 200,
+  },
+  "-=0.5"
+)
+  .from(".banner-content-left", {
+    autoAlpha: 0,
+    duration: 1,
+    y: -50,
+  })
+  .from(
+    ".banner-content-right",
+    {
+      autoAlpha: 0,
+      duration: 1,
+      y: -50,
+    },
+    "-=0.5"
+  )
+  .from(".profile-border", {
+    autoAlpha: 0,
+    duration: 1,
+    y: 50,
+  })
+  .from(".timeline-education", {
+    duration: 0.5,
+    autoAlpha: 0,
+    y: 50,
+    scrollTrigger: {
+      trigger: ".timeline-education",
+      start: "top 80%",
+      end: "top 60%",
+      scrub: 0.2,
+    },
+  })
+  .from(".timeline-work", {
+    duration: 0.5,
+    autoAlpha: 0,
+    y: 50,
+    scrollTrigger: {
+      trigger: ".timeline-work",
+      start: "top 80%",
+      end: "top 60%",
+      scrub: 0.2,
+    },
+  })
+  .from(".my-skills-header", {
+    duration: 0.5,
+    autoAlpha: 0,
+    y: 50,
+    scrollTrigger: {
+      trigger: ".my-skills-header",
+      start: "top 80%",
+      end: "top 60%",
+      scrub: 0.2,
+    },
+  })
+  .from(".my-skills-content-item", {
+    autoAlpha: 0,
+    y: 50,
+    ease: "power2.inOut",
+    scrollTrigger: {
+      trigger: ".my-skills-content",
+      start: "top 70%",
+      end: "bottom-=70% 30%",
+      scrub: 0.2,
+    },
+    stagger: {
+      from: "start",
+      amount: 0.5,
+      grid: "auto",
+      axis: "y",
+    },
+  })
+  .from(".my-projects-header-content", {
+    duration: 0.5,
+    autoAlpha: 0,
+    y: 50,
+    scrollTrigger: {
+      trigger: ".my-projects-header-content",
+      start: "top 80%",
+      end: "top 60%",
+      scrub: 0.2,
+    },
+  })
+  .from(".my-projects-content-item", {
+    autoAlpha: 0,
+    y: 50,
+    ease: "power3.inOut",
+    scrollTrigger: {
+      trigger: ".my-projects-content",
+      start: "top 70%",
+      end: "bottom-=50% 30%",
+      scrub: 0.2,
+    },
+    stagger: {
+      from: "start",
+      amount: 0.5,
+      grid: "auto",
+    },
+  })
+  .from(".contact-us-content", {
+    duration: 0.5,
+    autoAlpha: 0,
+    y: 50,
+    scrollTrigger: {
+      trigger: ".contact-us-content",
+      start: "top 80%",
+      end: "top 60%",
+      scrub: 0.2,
+    },
+  })
+  .from(".contact-us-content-left", {
+    duration: 0.5,
+    autoAlpha: 0,
+    y: 50,
+    scrollTrigger: {
+      trigger: ".contact-us-content",
+      start: "top 50%",
+      end: "top 30%",
+      scrub: 0.2,
+    },
+  })
+  .from(".contact-us-content-right", {
+    duration: 0.5,
+    autoAlpha: 0,
+    y: 50,
+    scrollTrigger: {
+      trigger: ".contact-us-content",
+      start: "top 50%",
+      end: "top 30%",
+      scrub: 0.2,
+    },
+  });
